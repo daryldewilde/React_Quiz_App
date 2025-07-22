7c97f102540f25c325757de333af5ab848f5f840
Nfoye Daryl <maviance.daryl.nfoye@gmail.com>
2025-07-22 12:27:50 +0100

    fix: add cursor pointer to button component for better user interaction

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ export default function Button({text, ...props}:ButtonComponentProps){
     return(
         <button 
             {...props} 
-            className="bg-pink-600 text-white px-4 md:px-6 py-2 md:py-3 rounded-lg font-medium hover:bg-pink-700 text-sm md:text-base"
+            className="bg-pink-600 text-white px-4 md:px-6 py-2 md:py-3 rounded-lg font-medium hover:bg-pink-700 text-sm md:text-base cursor-pointer"
         >
             {text}
         </button>
