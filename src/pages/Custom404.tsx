0bfe0c9451b72e11fc32032615a530abc66fb853
Nfoye Daryl <maviance.daryl.nfoye@gmail.com>
2025-07-24 16:04:32 +0100

    feat: implement custom 404 page with theme-based styling and add Vercel configuration for rewrites

--- src/pages/Custom404.tsx
+++ src/pages/Custom404.tsx
@@ -4,14 +4,19 @@ import { Typography, Box } from "@mui/material";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
+import { useThemeContext } from "../hooks/useThemeContext";
 
 export default function Custom404(){
     const navigate = useNavigate()
+    const themeContext = useThemeContext()
+
     return(
         <>
             <Header/>
             <Main>
-                <Box className="flex flex-col items-center justify-center gap-4">
+                <Box className={`flex flex-col items-center justify-center gap-4 ${
+                        themeContext.theme === "dark" ? "text-white" : "text-gray-900"
+                     }`}>
                     <Typography variant="body1" >404</Typography>
                     <Typography variant="body1" >OOPS we cannot find the page you are looking for</Typography>
                     <Button text="Go back to home page" onClick={()=>{navigate("/")}} />
